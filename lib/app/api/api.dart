e68fee066b8eccac59c29489399c32a293e594eb
opas-keat <opas.keat@gmail.com>
2023-11-11 21:47:56 +0700

    update baseUrl

--- lib/app/api/api.dart
+++ lib/app/api/api.dart
@@ -6,7 +6,7 @@ class Api {
   static const int connectionTimeout = 15000;
   // static const baseUrl = "https://pp-salesite.com";
   // static const baseUrl = "http://127.0.0.1:9999";
-  static const baseUrl = "https://warranty.ppsuperwheels.com";
+  static const baseUrl = "https://warranty-ppadmin.ppsuperwheels.com";
   static const baseUrlQr = "https://warranty.ppsuperwheels.com/#";
   static const baseUrlSystemLink = "https://pp-salesite.com/api/v1";
   // static const baseUrlSystemLink = "http://localhost:8081/api/v1";
